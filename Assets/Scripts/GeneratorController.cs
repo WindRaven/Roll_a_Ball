c8d16b03eee11f702a8fde0e1cf4a0f28146d2a3
Stefan-Lucian Bucur <slbucur@gmail.com>
2014-06-10 19:16:23 +0300

    Added friend object generator
    Added a ramp that generates a limited capacity of friend objects (also
    programmable)

---
+++ Assets/Scripts/GeneratorController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneratorController : MonoBehaviour {
+	public GameObject piece;
+	public float timeBetween;
+	public int capacity;
+
+	private float tick;
+	private float err = 0.1f;
+	private List<GameObject> pieces;
+
+
+	bool floatDivisible(float a, float b, float err){
+		if( (a/b - (int) (a/b)) % 1 < err)
+			return true;
+		return false;
+	}
+	// Use this for initialization
+	void Start () {
+		pieces = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(pieces.Count >= capacity)
+			return;
+
+		float time = Time.timeSinceLevelLoad;
+		if(time > tick)
+			tick = time;
+
+		if(floatDivisible(tick, timeBetween, err)){
+			tick++;
+			GameObject go = (GameObject) Instantiate(piece, 
+			                transform.position + Vector3.up * 6,
+			                Quaternion.identity);
+			go.transform.parent = this.transform.parent.transform;
+			//go.rigidbody.mass = 0.00001f;
+			pieces.Add(go);
+
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -54,8 +54,6 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		rigidbody.AddForce(movement * speed * Time.deltaTime);
-
-
 	}
 
 	void OnTriggerEnter(Collider other) {
